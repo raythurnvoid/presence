01968fbaf3439498fa05bb96369525b2d1ce3794
raythurnvoid <53383860+raythurnvoid@users.noreply.github.com>
2026-03-06 06:14:23 +0000

    Make the component testable

--- src/test.ts
+++ src/test.ts
@@ -2,6 +2,7 @@
 import type { TestConvex } from "convex-test";
 import type { GenericSchema, SchemaDefinition } from "convex/server";
 import schema from "./component/schema.js";
+
 const modules = import.meta.glob("./component/**/*.ts");
 
 /**
@@ -15,4 +16,5 @@ export function register(
 ) {
   t.registerComponent(name, schema, modules);
 }
+
 export default { register, schema, modules };
